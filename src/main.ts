4d6c422c9f45428b00b6cf15d38ff5a9518df811
LaylaR <layla.richard.2111@gmail.com>
2023-10-13 14:15:09 +0200

    add bootstrap add gith page and deploy config

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 import './style.css'
+
+// Import all of Bootstrap's JS
+// @ts-ignore
+import * as bootstrap from 'bootstrap'
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+﻿import path from "path";
+
+export default {
+    base: "/TechTest",
+    root: path.resolve(__dirname, ''),
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    }
+}
